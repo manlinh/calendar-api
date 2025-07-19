2ddfa19b7968e28927c305af927f0333cea585c1
manlinh <135205672+manlinh@users.noreply.github.com>
2025-07-19 12:34:21 +0800

    Update update-calendar.js

--- api/update-calendar.js
+++ api/update-calendar.js
@@ -1,4 +1,7 @@
-
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+if (req.method === 'OPTIONS') return res.status(200).end();
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).send("Only POST");
